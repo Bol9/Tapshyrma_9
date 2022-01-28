43a4e35246ea193366e8ebeb5f7131f905b756bf
KALYS'S LAPTOP <bekmenbetov@gmail.com>
2022-01-29 00:52:53 +0600

    commit one

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(san());
+
+	// write your code here
+
+    }
+    static int san(){
+        int sum=0;
+        for(int i=1; i<=300;i++){
+            if(i%3==0 && i%4==0 && i%5==0){
+                sum+=i;
+                //System.out.print(i+" ");
+
+            }
+
+        }
+        return sum;
+    }
+}
