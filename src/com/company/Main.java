346033f23e98c65e91144c6d5eeb434c9ea4fb93
KALYS'S LAPTOP <bekmenbetov@gmail.com>
2022-01-29 01:03:19 +0600

    f commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1-300 arasyndagy sandardyn 3,4,5 ke bolungon sanardy tabat");
+        System.out.println("1-300 arasyndagy sandardyn 3,4,5 ke bolungon sanardy tabat   ");
         System.out.println(san());
 
 	// write your code here
